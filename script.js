350d448c4478875d7769cf55859ca7ea7d176d41
Nishal-15 <nishalnishith@gmail.com>
2025-06-06 15:13:14 +0530

    javascript

---
+++ script.js
@@ -0,0 +1,22 @@
+console.log(5==='5');
+let i=0
+do{
+    i++;
+    console.log(i);
+}
+while(i<5)
+    
+function greet(name,age){
+return  `Hello ${name} is ${age} years old`
+}
+console.log(greet('Nishal',18));
+function add(a,b)
+{
+    return a+b
+}
+console.log(add(7,5));
+function game(userChoice){
+            const choices=['rock','paper','scissor']
+            let computer=choices[Math.floor(Math.random()*3)]
+            console.log(computer);
+        }
